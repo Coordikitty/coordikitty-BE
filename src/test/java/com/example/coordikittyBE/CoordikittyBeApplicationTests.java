ab5fd69882b223cfb99920d3f124fafab8f60d00
Tentenball <86109399+Tentennball@users.noreply.github.com>
2024-03-18 16:02:07 +0900

    Feat: execute initializer

---
+++ src/test/java/com/example/coordikittyBE/CoordikittyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coordikittyBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordikittyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
