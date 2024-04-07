c1022d83301b198278988a424463a53218ce239a
hotteok <hojun7889@gmail.com>
2024-04-07 18:54:17 +0900

    SCRUM-78 [Entity] alarm

---
+++ src/main/java/com/example/coordikittyBE/page/alarm/entity/AlarmEntity.java
@@ -0,0 +1,29 @@
+package com.example.coordikittyBE.page.alarm.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity(name = "alarm")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class AlarmEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID alarmId;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "time", nullable = false)
+    private String time;
+}
