f597fdfa5a9ea3f7d69c7dbda00b2992da5bc34e
hotteok00 <hojun7889@gmail.com>
2024-05-04 10:21:14 +0900

    SCRUM-35 Feat: FollowRequestDTO

---
+++ src/main/java/Coordinate/coordikittyBE/domain/follow/data/dto/FollowRequestDTO.java
@@ -0,0 +1,12 @@
+package Coordinate.coordikittyBE.domain.follow.data.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FollowRequestDTO {
+    private String email;
+}
