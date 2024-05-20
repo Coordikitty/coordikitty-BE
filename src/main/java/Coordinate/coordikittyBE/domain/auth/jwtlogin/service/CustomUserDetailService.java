eeaf73ebe52b731fcbb9a11541c8b0de2fbbe760
tentenball <lth8905@gmail.com>
2024-05-20 17:35:35 +0900

    SCRUM-152 feat: jwt로그인 구현2

---
+++ src/main/java/Coordinate/coordikittyBE/domain/auth/jwtlogin/service/CustomUserDetailService.java
@@ -0,0 +1,36 @@
+package Coordinate.coordikittyBE.domain.auth.jwtlogin.service;
+
+import Coordinate.coordikittyBE.domain.auth.entity.UserEntity;
+import Coordinate.coordikittyBE.domain.auth.repository.AuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return authRepository.findByUsername(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
+    }
+
+    // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
+    private UserDetails createUserDetails(UserEntity userEntity) {
+        return User.builder()
+                .username(userEntity.getUsername())
+                .password(passwordEncoder.encode(userEntity.getPassword()))
+                .roles(userEntity.getRoles().toArray(new String[0]))
+                .build();
+    }
+
+}
