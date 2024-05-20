5e3d4974e8f2514727e4a8beb339a4f99f20d90e
tentenball <lth8905@gmail.com>
2024-05-20 17:35:27 +0900

    SCRUM-152 feat: jwt로그인 구현

--- src/main/java/Coordinate/coordikittyBE/domain/auth/repository/AuthRepository.java
+++ src/main/java/Coordinate/coordikittyBE/domain/auth/repository/AuthRepository.java
@@ -3,5 +3,8 @@ package Coordinate.coordikittyBE.domain.auth.repository;
 import Coordinate.coordikittyBE.domain.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByUsername(String email);
 }
--- src/main/java/Coordinate/coordikittyBE/domain/closet/repository/ClothRepository.java
+++ src/main/java/Coordinate/coordikittyBE/domain/closet/repository/ClothRepository.java
@@ -1,10 +1,12 @@
 package Coordinate.coordikittyBE.domain.closet.repository;
 
+import Coordinate.coordikittyBE.domain.auth.entity.UserEntity;
 import Coordinate.coordikittyBE.domain.closet.entity.ClothEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ClothRepository extends JpaRepository<ClothEntity, UUID> {
-
+    List<ClothEntity> findAllByUserEntity(UserEntity userEntity);
 }
