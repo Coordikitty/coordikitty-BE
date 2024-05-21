c91f1f78e5174783b7eeab1b449c4d9af7a302b1
tentenball <lth8905@gmail.com>
2024-05-22 00:14:16 +0900

    Refact: PostlikeService 수정 완료(user만 남음)

--- src/main/java/Coordinate/coordikittyBE/domain/post/postlike/service/PostlikeService.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/postlike/service/PostlikeService.java
@@ -1,5 +1,6 @@
 package Coordinate.coordikittyBE.domain.post.postlike.service;
 
+import Coordinate.coordikittyBE.domain.post.entity.PostEntity;
 import Coordinate.coordikittyBE.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +13,14 @@ import java.util.UUID;
 public class PostlikeService {
     private final PostRepository postRepository;
     public ResponseEntity<String> like(UUID postId) {
-        var findPost = postRepository.findById(postId)
+        PostEntity findPost = postRepository.findById(postId)
                 .orElseThrow(()->new RuntimeException("잘못된 좋아요 요청"));
-
         findPost.setLikeCount(findPost.getLikeCount()+1);
         return ResponseEntity.ok("좋아요 성공");
     }
     public ResponseEntity<String> dislike(UUID postId) {
-        var findPost = postRepository.findById(postId)
+        PostEntity findPost = postRepository.findById(postId)
                 .orElseThrow(()->new RuntimeException("잘못된 좋아요 요청"));
-
         findPost.setLikeCount(findPost.getLikeCount()-1);
         return ResponseEntity.ok("좋아요 성공");
     }
