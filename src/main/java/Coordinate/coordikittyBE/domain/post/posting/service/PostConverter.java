46d2e3f6edb4e886827e06a79231c314da8abec8
tentenball <lth8905@gmail.com>
2024-05-30 09:05:09 +0900

    refact: postresponseDto 오류 수정

--- src/main/java/Coordinate/coordikittyBE/domain/post/posting/service/PostConverter.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/posting/service/PostConverter.java
@@ -1,14 +1,10 @@
 package Coordinate.coordikittyBE.domain.post.posting.service;
 
 
-import Coordinate.coordikittyBE.domain.closet.entity.ClothEntity;
 import Coordinate.coordikittyBE.domain.post.entity.PostEntity;
 import Coordinate.coordikittyBE.domain.post.posting.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +21,6 @@ public class PostConverter {
                 .uploaderNickname(post.getUserEntity().getNickname())
                 .uploaderProfileImg(post.getUserEntity().getProfileUrl())
                 //.postImgs(post.getPostImgs())
-                .cloth(null)
                 .isLiked(true)
                 .isBookmarked(true)
                 .build();
