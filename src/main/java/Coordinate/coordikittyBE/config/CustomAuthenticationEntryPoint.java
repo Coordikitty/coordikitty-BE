083f1713c167543ae79791ab701c9e7bcfd89e15
tentenball <lth8905@gmail.com>
2024-06-01 17:40:26 +0900

    refact: SecureConfig 오류핸들러 수정

--- src/main/java/Coordinate/coordikittyBE/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/Coordinate/coordikittyBE/config/CustomAuthenticationEntryPoint.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.sendError(response.getStatus(), authException.getMessage());
     }
 }
