13395838fa9f2f6469ef87f4daf2d999041024d0
tentenball <lth8905@gmail.com>
2024-06-26 01:31:15 +0900

    refact: cicd setting file 수정 및 쉘파일 생성

---
+++ scripts/gh_deploy.sh
@@ -0,0 +1,25 @@
+REPOSITORY=/home/ubuntu/coordikitty
+cd $REPOSITORY
+
+APP_NAME=coordikitty-backend
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH에 실행권한 추가"
+chmod +x $JAR_PATH
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+echo nohup.out 2>$1 &
