5af32b4ef035e7061b2ec31fc6ba9300708c9f58
hotteok <hojun7889@gmail.com>
2024-07-01 19:30:53 +0900

    Fix: RecommendService url 수정

--- src/main/java/Coordinate/coordikittyBE/domain/recommend/service/RecommendService.java
+++ src/main/java/Coordinate/coordikittyBE/domain/recommend/service/RecommendService.java
@@ -38,7 +38,7 @@ public class RecommendService {
     @Value("${openweathermap.key}")
     private String apiKey;
     public List<RecommendGetResponseDto> getRecommend(String email, Type type, String value, CoordinatesDto coordinatesDto) {
-        String url = "https://2f75-119-201-76-250.ngrok-free.app";
+        String url = "https://2f75-119-201-76-250.ngrok-free.app/recommend";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         List<Cloth> clothes = clothRepository.findAllByUserEmailAndStyle(email, Style.valueOf(value));
