9ed288c5c9c858eac228ac14a9a734d748f1470a
tentenball <lth8905@gmail.com>
2024-07-04 01:18:33 +0900

    SCRUM-198 refact: ClosetPostResponseDto to record

--- src/main/java/Coordinate/coordikittyBE/domain/closet/dto/ClosetPostRequestDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/closet/dto/ClosetPostRequestDto.java
@@ -1,24 +1,13 @@
 package Coordinate.coordikittyBE.domain.closet.dto;
 
 import Coordinate.coordikittyBE.domain.closet.enums.*;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ClosetPostRequestDto {
-    private Category.Large large;
-    private Category.Medium medium;
-    private Category.Small small;
-    private Fit fit;
-    private Gender gender;
-    private Style style;
-    private Thickness thickness;
-
+public record ClosetPostRequestDto(
+        Category.Large large,
+        Category.Medium medium,
+        Category.Small small,
+        Fit fit,
+        Gender gender,
+        Style style,
+        Thickness thickness) {
 }
