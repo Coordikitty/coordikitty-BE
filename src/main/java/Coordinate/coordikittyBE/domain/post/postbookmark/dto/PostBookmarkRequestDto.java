cae9bc9cc380ed739397bbccb9e6a0e016cf0619
tentenball <lth8905@gmail.com>
2024-07-04 23:00:35 +0900

    SCRUM-198 refact: PostBookmarkRequestDto to record

--- src/main/java/Coordinate/coordikittyBE/domain/post/postbookmark/dto/PostBookmarkRequestDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/postbookmark/dto/PostBookmarkRequestDto.java
@@ -1,14 +1,7 @@
 package Coordinate.coordikittyBE.domain.post.postbookmark.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class PostBookmarkRequestDto {
-    private UUID postId;
+public record PostBookmarkRequestDto(UUID postId) {
 }
