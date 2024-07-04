b91f76a189afc2bb88389d9163fd47d15a18f646
tentenball <lth8905@gmail.com>
2024-07-05 00:31:49 +0900

    SCRUM-198 refact: 프로필image dto to record

--- src/main/java/Coordinate/coordikittyBE/domain/settings/image/dto/SettingImageResponseDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/settings/image/dto/SettingImageResponseDto.java
@@ -1,12 +1,5 @@
 package Coordinate.coordikittyBE.domain.settings.image.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class SettingImageResponseDto {
-    private String profileURL;
+public record SettingImageResponseDto(String profileURL) {
 }
