4a3e50ae49254848c928b6c336186876fd5c358f
tentenball <lth8905@gmail.com>
2024-07-05 00:34:55 +0900

    SCRUM-198 refact: SettingProfile...Dto to record

--- src/main/java/Coordinate/coordikittyBE/domain/settings/profile/dto/SettingProfileRequestDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/settings/profile/dto/SettingProfileRequestDto.java
@@ -1,12 +1,5 @@
 package Coordinate.coordikittyBE.domain.settings.profile.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class SettingProfileRequestDto {
-    private String nickname;
+public record SettingProfileRequestDto(String nickname) {
 }
--- src/main/java/Coordinate/coordikittyBE/domain/settings/profile/dto/SettingProfileResponseDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/settings/profile/dto/SettingProfileResponseDto.java
@@ -1,23 +1,9 @@
 package Coordinate.coordikittyBE.domain.settings.profile.dto;
 
 import Coordinate.coordikittyBE.domain.auth.entity.User;
-import lombok.*;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class SettingProfileResponseDto {
-    private String name;
-    private String number;
-    private String nickname;
-
+public record SettingProfileResponseDto(String name, String phoneNumber, String nickname) {
     public static SettingProfileResponseDto from(User user) {
-        return SettingProfileResponseDto.builder()
-                .name(user.getName())
-                .number(user.getPhoneNumber())
-                .nickname(user.getNickname())
-                .build();
+        return new SettingProfileResponseDto(user.getName(), user.getPhoneNumber(), user.getNickname());
     }
 }
