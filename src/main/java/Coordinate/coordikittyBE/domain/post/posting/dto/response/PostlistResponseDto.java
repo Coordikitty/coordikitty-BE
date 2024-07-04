580383912ba5c884e453479f4d08b402c64d7263
tentenball <lth8905@gmail.com>
2024-07-04 23:28:58 +0900

    SCRUM-198 refact: PostlistResponseDto to record

--- src/main/java/Coordinate/coordikittyBE/domain/post/posting/dto/response/PostlistResponseDto.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/posting/dto/response/PostlistResponseDto.java
@@ -3,44 +3,38 @@ package Coordinate.coordikittyBE.domain.post.posting.dto.response;
 import Coordinate.coordikittyBE.domain.closet.enums.Style;
 import Coordinate.coordikittyBE.domain.history.entity.History;
 import Coordinate.coordikittyBE.domain.post.entity.Post;
-import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Data
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class PostlistResponseDto {
-    private UUID postId;
-    private String content;
-    private Style style;
-    private int postLike;
-    private LocalDateTime uploadDate;
-    private String uploaderEmail;
-    private String uploaderNickname;
-    private String uploaderProfileImg;
-    private Boolean isLiked;
-    private Boolean isBookmarked;
-    private List<String> postImgs;
 
+public record PostlistResponseDto (
+        UUID postId,
+        String content,
+        Style style,
+        int postLike,
+        LocalDateTime uploadDate,
+        String uploaderEmail,
+        String uploaderNickname,
+        String uploaderProfileImg,
+        Boolean isLiked,
+        Boolean isBookmarked,
+        List<String> postImgs
+){
     public static PostlistResponseDto of(Post post, History history) {
-        return PostlistResponseDto.builder()
-                .postId(post.getId())
-                .content(post.getContent())
-                .style(post.getStyle())
-                .postLike(post.getLikeCount())
-                .postImgs(post.getPostImgs())
-                .uploadDate(post.getCreatedAt())
-                .uploaderEmail(post.getUser().getEmail())
-                .uploaderNickname(post.getUser().getNickname())
-                .uploaderProfileImg(post.getUser().getProfileUrl())
-                .isLiked(history.getIsLiked())
-                .isBookmarked(history.getIsBookmarked())
-                .build();
+        return new PostlistResponseDto(
+                post.getId(),
+                post.getContent(),
+                post.getStyle(),
+                post.getLikeCount(),
+                post.getCreatedAt(),
+                post.getUser().getEmail(),
+                post.getUser().getNickname(),
+                post.getUser().getProfileUrl(),
+                history.getIsLiked(),
+                history.getIsBookmarked(),
+                post.getPostImgs()
+        );
     }
 }
