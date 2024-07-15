0759de10fd0ab563f2dd5feac2738688a2b97923
tentenball <lth8905@gmail.com>
2024-07-15 23:28:54 +0900

    SCRUM-217 comment: jwt필터 주석제거

--- src/main/java/Coordinate/coordikittyBE/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/Coordinate/coordikittyBE/security/jwt/JwtAuthenticationFilter.java
@@ -19,18 +19,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 1. Request Header에서 JWT 토큰 추출
         String token = resolveToken(request);
 
-        // 2. validateToken으로 토큰 유효성 검사
         if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         chain.doFilter(request, response);
     }
-    // Request Header에서 토큰 정보 추출
+
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
