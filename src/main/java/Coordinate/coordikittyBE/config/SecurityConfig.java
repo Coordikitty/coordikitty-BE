7477e4f88157c61938bd294904e12ac8e2058386
tentenball <lth8905@gmail.com>
2024-07-30 17:11:55 +0900

    CD-226 feat: 시큐리티 jwt오류처리를 위한 필터 구현

--- src/main/java/Coordinate/coordikittyBE/config/SecurityConfig.java
+++ src/main/java/Coordinate/coordikittyBE/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package Coordinate.coordikittyBE.config;
 
 import Coordinate.coordikittyBE.domain.security.jwt.JwtAuthenticationFilter;
+import Coordinate.coordikittyBE.domain.security.jwt.JwtExceptionFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -14,8 +16,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationEntryPoint authenticationEntryPoint;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
+    private final JwtExceptionFilter jwtExceptionFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -34,7 +37,9 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
+                .exceptionHandling(exception-> exception.authenticationEntryPoint(authenticationEntryPoint))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/Coordinate/coordikittyBE/config/SecurityFilterConfig.java
@@ -0,0 +1,33 @@
+package Coordinate.coordikittyBE.config;
+
+import Coordinate.coordikittyBE.domain.security.jwt.JwtAuthenticationEntryPoint;
+import Coordinate.coordikittyBE.domain.security.jwt.JwtAuthenticationFilter;
+import Coordinate.coordikittyBE.domain.security.jwt.JwtExceptionFilter;
+import Coordinate.coordikittyBE.global.util.JwtHelper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityFilterConfig {
+    private final JwtHelper jwtHelper;
+    private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
+    @Bean
+    public JwtAuthenticationEntryPoint authenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint(objectMapper);
+    }
+
+    @Bean
+    public JwtExceptionFilter jwtExceptionFilter(){
+        return new JwtExceptionFilter(objectMapper);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(jwtHelper, userDetailsService);
+    }
+}
