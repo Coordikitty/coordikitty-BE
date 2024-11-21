5e49a92df27596fc23e1f62f1db0ba88a000a971
tentenball <lth8905@gmail.com>
2024-11-22 02:15:59 +0900

    refact: history 로직 수정

--- src/main/java/Coordinate/coordikittyBE/domain/post/repository/PostRepository.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/repository/PostRepository.java
@@ -22,7 +22,7 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByUserEmailOrderByCreatedAtDesc(@Param("email") String email);
 
     @Query("SELECT p FROM post p JOIN FETCH p.user JOIN FETCH p.historys h"
-        + " WHERE p.user.email = :email AND h.isBookmarked = :isBookmarked ORDER BY p.createdAt DESC")
+        + " WHERE h.user.email = :email AND h.isBookmarked = :isBookmarked ORDER BY p.createdAt DESC")
     List<Post> findAllByEmailAndIsBookmarkedOrderByCreatedAtDesc(
         @Param("email") String email, @Param("isBookmarked") boolean isBookmarked
     );
