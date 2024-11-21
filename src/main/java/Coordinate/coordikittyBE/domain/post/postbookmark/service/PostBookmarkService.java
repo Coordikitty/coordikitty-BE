531c0368b5a52a0516cb50b761e31d3119464dd1
tentenball <lth8905@gmail.com>
2024-11-22 01:20:49 +0900

    refact: 게시글 좋아요 로직 수정

--- src/main/java/Coordinate/coordikittyBE/domain/post/postbookmark/service/PostBookmarkService.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/postbookmark/service/PostBookmarkService.java
@@ -1,19 +1,19 @@
 package Coordinate.coordikittyBE.domain.post.postbookmark.service;
 
-import Coordinate.coordikittyBE.domain.user.entity.User;
-import Coordinate.coordikittyBE.domain.user.repository.UserRepository;
 import Coordinate.coordikittyBE.domain.history.entity.History;
 import Coordinate.coordikittyBE.domain.history.repository.HistoryRepository;
 import Coordinate.coordikittyBE.domain.post.entity.Post;
 import Coordinate.coordikittyBE.domain.post.repository.PostRepository;
+import Coordinate.coordikittyBE.domain.user.entity.User;
+import Coordinate.coordikittyBE.domain.user.repository.UserRepository;
 import Coordinate.coordikittyBE.exception.CoordikittyException;
 import Coordinate.coordikittyBE.exception.ErrorType;
 import Coordinate.coordikittyBE.global.common.response.SuccessResponse;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 @Service
 @Transactional
@@ -23,22 +23,23 @@ public class PostBookmarkService {
     private final UserRepository userRepository;
     private final HistoryRepository historyRepository;
 
-    public SuccessResponse<String> bookmark(UUID postId, String email) {
-        // history 에 bookmark 추가
-        // 원래 있던 history 면 북마크 true
-        // 없던 history 면 history 생성 후 북마크 true
+    public SuccessResponse<?> bookmark(UUID postId, String email) {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new CoordikittyException(ErrorType.MEMBER_NOT_FOUND));
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new CoordikittyException(ErrorType.POST_NOT_FOUND));
 
-        User user = userRepository.findByEmail(email).orElseThrow(()-> new CoordikittyException(ErrorType.MEMBER_NOT_FOUND));
-        Post post = postRepository.findById(postId).orElseThrow(()-> new CoordikittyException(ErrorType.POST_NOT_FOUND));
-
-        History history = historyRepository.findByUserIdAndPostId(user.getId(), postId)
-                .orElseGet(() -> History.of(user, post));
-
-        history.toggleBookmarked();
-        if(history.getIsLiked()){
-            return SuccessResponse.from("북마크 추가 성공");
+        Optional<History> history = historyRepository.findByUserIdAndPostId(post.getUser().getId(), postId);
+        if (history.isPresent()) {
+            history.get().toggleBookmarked();
+            return SuccessResponse.from(history.get().getIsBookmarked());
         }
-       return SuccessResponse.from("북마크 해제 성공");
+
+        History newHistory = History.of(user, post);
+        newHistory.toggleBookmarked();
+        historyRepository.save(newHistory);
+
+        return SuccessResponse.from(newHistory.getIsBookmarked());
     }
 
 }
--- src/main/java/Coordinate/coordikittyBE/domain/post/postlike/service/PostlikeService.java
+++ src/main/java/Coordinate/coordikittyBE/domain/post/postlike/service/PostlikeService.java
@@ -9,6 +9,7 @@ import Coordinate.coordikittyBE.domain.post.repository.PostRepository;
 import Coordinate.coordikittyBE.exception.CoordikittyException;
 import Coordinate.coordikittyBE.exception.ErrorType;
 import Coordinate.coordikittyBE.global.common.response.SuccessResponse;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,23 +25,32 @@ public class PostlikeService {
     private final UserRepository userRepository;
 
     @Transactional
-    public SuccessResponse<String> like(UUID postId, String email) {
+    public SuccessResponse<?> like(UUID postId, String email) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new CoordikittyException(ErrorType.POST_NOT_FOUND));
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new CoordikittyException(ErrorType.MEMBER_NOT_FOUND));
-        History history = historyRepository.findByUserIdAndPostId(user.getId(), postId)
-                .orElseGet(() -> History.of(user, post));
-
-        history.toggleLike();
-        System.out.println(history.getIsLiked());
-        if (history.getIsLiked()){
-            post.like();
-            return SuccessResponse.from("좋아요 성공");
+        Optional<History> history = historyRepository.findByUserIdAndPostId(user.getId(), postId);
+        if (history.isPresent()) {
+            history.get().toggleLike();
+            upOrDownPostLikeCount(history.get(), post);
+            return SuccessResponse.from(post.getLikeCount());
         }
 
+        History newHistory = History.of(user, post);
+        newHistory.toggleLike();
+        upOrDownPostLikeCount(newHistory, post);
+        historyRepository.save(newHistory);
+
+        return SuccessResponse.from(post.getLikeCount());
+    }
+
+    private void upOrDownPostLikeCount(History history, Post post) {
+        if(history.getIsLiked()){
+            post.like();
+            return;
+        }
         post.unlike();
-        return SuccessResponse.from("좋아요 취소 성공");
     }
 
 }
